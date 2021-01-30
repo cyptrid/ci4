45ac2c7dd46fc58e1728628e94d9ddec71bee81b
cyptrid <cyptrid@gmail.com>
2021-01-30 20:33:58 +0700

    Insert Data

---
+++ app/Views/komik/create.php
@@ -0,0 +1,45 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h2>Form Tambah Data Komik</h2>
+            <form action="/komik/save" method="post">
+                <?= csrf_field(); ?>
+                <div class="form-group row">
+                    <label for="judul" class="col-sm-2 col-form-label">Judul</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" name="judul" id="judul" autofocus>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="penulis" class="col-sm-2 col-form-label">Penulis</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" name="penulis" id="penulis">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="penerbit" class="col-sm-2 col-form-label">Penerbit</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" name="penerbit" id="penerbit">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="sampul" class="col-sm-2 col-form-label">Sampul</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" name="sampul" id="sampul">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-sm-10">
+                        <button type="submit" class="btn btn-primary">Tambah Data</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<?= $this->endSection(); ?>
