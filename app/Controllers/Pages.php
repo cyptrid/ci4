98a7a1007a56495429d723490c5c5f1cb91a724a
cyptrid <cyptrid@gmail.com>
2021-01-30 15:09:15 +0700

    View Layouts

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -7,9 +7,7 @@ class Pages extends BaseController
     public function index()
     {
         $data = ['title' => 'Home | Codeigniter 4'];
-        echo view('layout/header', $data);
-        echo view('pages/home');
-        echo view('layout/footer');
+        return view('pages/home', $data);
     }
 
     //--------------------------------------------------------------------
@@ -17,8 +15,28 @@ class Pages extends BaseController
     public function about()
     {
         $data = ['title' => 'About | Codeigniter 4'];
-        echo view('layout/header', $data);
-        echo view('pages/about');
-        echo view('layout/footer');
+        return view('pages/about', $data);
+    }
+
+    //--------------------------------------------------------------------
+
+    public function contact()
+    {
+        $data = [
+            'title' => 'Contact | Codeigniter 4',
+            'alamat' => [
+                [
+                    'tipe' => 'Rumah',
+                    'alamat' => 'JL. abc No. 123',
+                    'kota' => 'Bandung'
+                ],
+                [
+                    'tipe' => 'Apartment',
+                    'alamat' => 'JL. abc No. 123',
+                    'kota' => 'Bogoe'
+                ]
+            ]
+        ];
+        return view('pages/contact', $data);
     }
 }
